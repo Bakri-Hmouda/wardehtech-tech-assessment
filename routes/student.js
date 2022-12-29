880335416ca20aae1d31167233dab4fba9299fe4
Bakrifly <bakri.hmouda@gmail.com>
2022-12-29 22:11:02 +0200

    added auth middleWare to routes

--- routes/student.js
+++ routes/student.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
 const controller = require('../controllers/student')
+const auth = require('../middlewares/auth')
 
 /**
  * student routes
  */
-router.route('/').get(controller.getEntities) // get all students
-router.route('/:id').get(controller.getEntity) // get one student
-router.route('/create').post(controller.createEntity) // create a student
-router.route('/update/:id').put(controller.updateEntity) // update a student
-router.route('/delete/:id').delete(controller.deleteEntity) // delete a student
+router.route('/').get(auth, controller.getEntities) // get all students
+router.route('/:id').get(auth, controller.getEntity) // get one student
+router.route('/create').post(auth, controller.createEntity) // create a student
+router.route('/update/:id').put(auth, controller.updateEntity) // update a student
+router.route('/delete/:id').delete(auth, controller.deleteEntity) // delete a student
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,9 @@ const controller = require('../controllers/user')
 
 /**
  * user routes
+ * not auth has been used here, requirement are a bit unclear.
  */
-router.route('/login').post(controller.signIn) // signIn user
+router.route('/login').post(controller.login) // signIn user
 router.route('/logout').post(controller.logout) // logout user
 router.route('/register').post(controller.createEntity) // create a user
 router.route('/reset-pass').put(controller.updateEntity) // update a student
