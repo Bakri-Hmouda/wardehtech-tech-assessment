14408a89e925b439602712f42de917f66fd75e52
Bakrifly <bakri.hmouda@gmail.com>
2022-12-29 16:38:58 +0200

    added database connection

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ const app = express()
 const env = require('dotenv').config()
 const logger = require('morgan')
 const PORT = process.env.PORT || 5000
+const DB_URI = process.env.DB_URI
+const Mongoose = require('mongoose')
 
-// routes imports
+
+/**
+ * connect to database
+ */
+Mongoose.connect(DB_URI, () => console.log('connected successfully to database'))
+
+/**
+ * import routes
+ */
 const student_route = require('./routes/student')
 const user_route = require('./routes/user')
+// test route
+app.get('/test', (req, res) => res.json('app is working'))
+
 
 app.use('/student', student_route);
 app.use('/user', user_route);
 
+
+
+/**
+ * start server with the specified port in the ENV or port 5000
+ */
 app.listen(PORT, () => { console.log(`server up and running on port: ${PORT}`) })
